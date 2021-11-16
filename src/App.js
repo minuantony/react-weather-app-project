75762f199df6942c756291b3eebbe04bee140f8d
minuantony <minuantony19@gmail.com>
2021-11-16 10:34:51 -0600

    Add Git link to the code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/minuantony/react-weather-app-project"
+          target="_blank"
+        >
+          Link to Code
+        </a>
+      </footer>
+    </div>
+  );
 }
