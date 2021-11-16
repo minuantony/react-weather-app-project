d52022f1dc0506985b949df345f1835716aa25cb
minuantony <minuantony19@gmail.com>
2021-11-16 14:46:05 -0600

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        <a
-          href="https://github.com/minuantony/react-weather-app-project"
-          target="_blank"
-        >
-          Link to Code
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          <a
+            href="https://github.com/minuantony/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Link to Code
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
