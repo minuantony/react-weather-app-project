ba4567a62c53c969cd669be36ce44688e3f633a9
minuantony <minuantony19@gmail.com>
2021-11-17 15:51:46 -0600

    Added Basic Styling

--- src/App.js
+++ src/App.js
@@ -5,24 +5,21 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="wrap">
-          <h1> Weather App</h1>
-          <div className="row border">
-            <Weather />
-          </div>
-          <footer>
-            {" "}
-            This project was coded by Minu Antony and is{" "}
-            <a
-              href="https://github.com/minuantony/react-weather-app-project"
-              target="_blank"
-              rel="noreferrer"
-            >
-              open-sourced on GitHub
-            </a>{" "}
-            and hosted on Netlify
-          </footer>
+        <div className="row border">
+          <Weather />
         </div>
+        <footer>
+          {" "}
+          This project was coded by Minu Antony and is{" "}
+          <a
+            href="https://github.com/minuantony/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Netlify.
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -4,40 +4,42 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <div classNmae="row">
-        <form className="form mt-3">
-          <div className="row">
-            <div className="col-8">
-              <input
-                type="text"
-                placeholder="Type a city"
-                className="form-control"
-              />
-            </div>
-            <div className="col-2">
-              <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-          </div>
-        </form>
-        <h3>New York</h3>
-        <ul className="description">
-          <li>Tuesday 15:22</li>
-          <li>Broken Clouds</li>
-        </ul>
+      <form className="form mt-3">
         <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt=""
+          <div className="col-8">
+            <input
+              type="text"
+              placeholder="Type a city"
+              className="form-control"
+              autoFocus="on"
             />
-            9°C
           </div>
-          <div className="col-5">
-            <ul>
-              <li>Humidity: 44%</li>
-              <li>Wind: 0.45km/h</li>
-            </ul>
+          <div className="col-2">
+            <input type="submit" value="Search" className="btn btn-primary" />
           </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul className="description">
+        <li>Tuesday 15:22</li>
+        <li>Broken Clouds</li>
+      </ul>
+      <div className="row">
+        <div className="col-6 p-2">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt=""
+            width="64"
+            height="64"
+          />
+          <span className="temperature">9</span>
+          <span className="unit">°C|F</span>
+        </div>
+        <div className="col-5 p-2">
+          <ul>
+            <li>Humidity: 44%</li>
+            <li>Wind: 0.45km/h</li>
+          </ul>
         </div>
       </div>
     </div>
