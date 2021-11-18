781259729b89d3ae352d82a41b14fe7be8f31cba
minuantony <minuantony19@gmail.com>
2021-11-17 21:50:03 -0600

    Added API integration

--- src/Weathertest.js
+++ src/Weathertest.js
@@ -3,36 +3,23 @@ import "./Weather.css";
 import axios from "axios";
 
 export default function Weather() {
+  let [temperature, setTemperature] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  let [city, setCity] = useState("");
-  let [weatherData, setWeatherData] = useState({});
+let [weatherData, setWeatherData] = useState({});
 
   function showResponse(response) {
     console.log(response.data);
+    setTemperature(Math.round(response.data.main.temp));
     setWeatherData({
-      temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
-      date: "Wednesday 07:00",
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      city: response.data.name,
     });
-  }
-  function handleSubmit(event) {
-    event.preventDefault();
-    setLoaded(true);
-    let apiKey = "6f7afd3770eb32dd0f8cf4d22a38e396";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(showResponse);
-  }
-  function updateChange(event) {
-    setCity(event.target.value);
-  }
   if (loaded) {
+    let city = "London";
     return (
       <div className="Weather">
-        <form className="form mt-3" onSubmit={handleSubmit}>
+        <form className="form mt-3">
           <div className="row">
             <div className="col-8">
               <input
@@ -40,7 +27,6 @@ export default function Weather() {
                 placeholder="Type a city"
                 className="form-control"
                 autoFocus="on"
-                onChange={updateChange}
               />
             </div>
             <div className="col-2">
@@ -48,30 +34,39 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>{weatherData.city}</h1>
+        <h1>{city}</h1>
         <ul className="description">
-          <li>{weatherData.date}</li>
-          <li>{weatherData.description}</li>
+          <li>Tuesday 15:22</li>
+          <li>Broken Clouds</li>
         </ul>
         <div className="row">
           <div className="col-6 p-2">
-            <img src={weatherData.iconUrl} alt="" width="64" height="64" />
-            <span className="temperature">{weatherData.temperature}</span>
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt=""
+              width="64"
+              height="64"
+            />
+            <span className="temperature">{temperature}</span>
             <span className="unit">°C|F</span>
           </div>
           <div className="col-5 p-2">
             <ul>
-              <li>Humidity:{weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}km/h</li>
+              <li>Humidity: 44%</li>
+              <li>Wind: 0.45km/h</li>
             </ul>
           </div>
         </div>
       </div>
     );
   } else {
+    let city = "London";
+    let apiKey = "04dd91307ec56c5bdc7c8b53d6799c73";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showResponse);
     return (
       <div>
-        <form className="form mt-3" onSubmit={handleSubmit}>
+        <form className="form mt-3">
           <div className="row">
             <div className="col-8">
               <input
@@ -79,7 +74,6 @@ export default function Weather() {
                 placeholder="Type a city"
                 className="form-control"
                 autoFocus="on"
-                onChange={updateChange}
               />
             </div>
             <div className="col-2">
@@ -87,7 +81,7 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <br />
+        <h4>Loading..</h4>
       </div>
     );
   }
